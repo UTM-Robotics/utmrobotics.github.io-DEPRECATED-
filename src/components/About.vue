0ffabadf1bbb6f077b967284a20f3a2f9bd2c4b1
Julian-Sequeira <julian.sequeira@mail.utoronto.ca>
2019-11-12 23:14:13 -0500

    new build for the first draft

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,7 +7,7 @@
             The best way to keep in touch is via slack (there is a link in the top right of the page) 
         </v-row>
         <v-row align="center" justify="center">
-            You can also be added to our mailing list here: &nbsp; <a href="http://eepurl.com/gGaxl5">touch me</a>
+            You can also be added to our mailing list here: &nbsp; <a href="http://eepurl.com/gGaxl5"> (touch me)</a>
         </v-row>
     </v-container>
 </template>
